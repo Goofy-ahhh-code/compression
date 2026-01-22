a62c9f578e1dec2c9680d1efd6f4acab137f16f1
plainduck67 <plainduck67@gmail.com>
2026-01-21 21:28:30 -0700

    added file support and makefile

--- src/main.c
+++ src/main.c
@@ -2,55 +2,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int compress(char string[]) {
+int compress(char string[], const char *path) {
     char current_value = string[0];
     int length_num = 0;
     int length = stringlen(string);
+
+    FILE *write = fopen(path, "w");
+
+    if (!write) return -1;
+
     for (int i = 0; i <= length; i++) {
         if (string[i] == current_value) {
             length_num++;
         } else if (string[i] != current_value) {
-            printf("%c%d", string[i - 1], length_num);
+            fprintf(write, "%c%d", string[i - 1], length_num);
             length_num = 1;
             current_value = string[i];
         } else if (string[i] != string[i + 1]) {
-            printf("%c", string[i]);
+            fprintf(write, "%c", string[i]);
             length_num = 1;
         } else if (string[i] == '\0' || string[i] == '\n') {
-            printf("ended");
+            fclose(write);
             break;
         }
     }
     return 0;
 }
 
-int decompress(char string[]) {
+int decompress(char string[], const char *path) {
+
+
+    FILE *write = fopen(path, "w");
+
+    if (!write) return -1;
+
     int length = stringlen(string);
     int num_per_char;
     for (int i = 0; i <= length; i += 2) {
         if (string[i] != '\0') {
             num_per_char = string[i + 1] - '0';
             for (int j = 0; j < num_per_char; j++) {
-                printf("%c", string[i]);
+                fprintf(write, "%c", string[i]);
             }
         } else {
+            fclose(write);
             break;
         }
     }
     return 0;
 }
-int main() {
-    char input;
+
+int main(int argc, char *argv[]) {
     char string[200000];
-    printf("Would you like to compress or decompress");
-    scanf("%c", &input);
-    if (input == 'c') {
-        scanf("%s", string);
-        compress(string);
+
+    FILE *read = fopen(argv[2], "r");
+    if (!read) return -1;
+
+    fscanf(read, "%s", string);
+
+    if (stringcmp(argv[1], "compress") == 0) {
+        compress(string, argv[2]);
     }
-    if (input == 'd') {
-        scanf("%s", string);
-        decompress(string);
+     if (stringcmp(argv[1], "decompress") == 0) {
+        decompress(string, argv[2]);
     }
+
     return 0;
 }
